bdfcecf0f33ebbc4251bf3248adbe706c0577ff4
Mark Ethan Trostler <mark@zzo.com>
2015-05-15 10:18:46 -0700

    404

--- server.js
+++ server.js
@@ -171,6 +171,24 @@ app.get('/getFile/:username/:repo/:branch/:buildId/:buildNumber/:filename', func
   });
 });
 
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 console.log('Listening on %s', port);
 app.listen(port);
