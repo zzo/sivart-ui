119b53178681f7ad4d8a87db171b2a52a15e6d05
Mark Ethan Trostler <mark@zzo.com>
2015-06-07 10:09:27 -0700

    switch over github auth callback url

--- routes/Login.js
+++ routes/Login.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/github/callback"
+    callbackURL: "http://xci.pub/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     profile.accessToken = accessToken;
